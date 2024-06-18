092d32f14e3cd6355de6def8ed5d368f292c0036
TRenganathan <Iprotecs_React@PC2082IND.detasad.com>
2024-06-18 18:35:18 +0530

    bcryptjs

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const speakeasy = require("speakeasy");
 const { v4: uuidv4 } = require("uuid");
